20c06c9369d1c01e21eb3672f0d9badbff1bbac7
Sam Hill <sam.hill@informed.com>
2025-05-14 11:25:39 +0100

    Initial repo setup for NOVA

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <Typography variant="h4" gutterBottom>
+        Hello, World
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
