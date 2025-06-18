dfe076b5978b74d9f6540315305a1911e2c3a694
Sam Hill <sam.hill@informed.com>
2025-06-18 16:40:38 +0100

    Revert previous changes + modified backend dockerfile

--- api/src/utils/data-provider.utils.ts
+++ api/src/utils/data-provider.utils.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import Fuse from "fuse.js";
+import Fuse from 'fuse.js';
 import { LayersDTO } from "../models/layers.model";
 import { AssetsDTO } from "../models/asset.model";
 import { GeoJSONDTO } from "../models/geojson.model";
@@ -23,17 +23,11 @@ export class DataProviderUtils {
    * Constructor for DataProviderUtils
    */
   constructor() {
-    this.regionsDataFilePath = path.resolve(__dirname, "../data/regions.json");
-    this.layersDataFilePath = path.resolve(__dirname, "../data/layers.json");
-    this.assetsDataFilePath = path.resolve(__dirname, "../data/assets.json");
-    this.sampleGeoJsonFilePath = path.resolve(
-      __dirname,
-      "../data/sampleGeoJson.json"
-    );
-    this.substationsDataFilePath = path.resolve(
-      __dirname,
-      "../data/substations.json"
-    );
+    this.regionsDataFilePath = path.join(__dirname, '../data/regions.json');
+    this.layersDataFilePath = path.join(__dirname, "../data/layers.json");
+    this.assetsDataFilePath = path.join(__dirname, "../data/assets.json");
+    this.sampleGeoJsonFilePath = path.join(__dirname, "../data/sampleGeoJson.json");
+    this.substationsDataFilePath = path.join(__dirname, "../data/substations.json");
   }
 
   /**
@@ -41,12 +35,12 @@ export class DataProviderUtils {
    * @returns LayersDTO object containing the layers data
    */
   public readLayersData(): LayersDTO {
-    const fileContent = fs.readFileSync(this.layersDataFilePath, "utf8");
+    const fileContent = fs.readFileSync(this.layersDataFilePath, 'utf8');
     const layersData = JSON.parse(fileContent) as LayersDTO;
 
     // Ensure each item has the active property
-    layersData.categories.forEach((category) => {
-      category.items.forEach((item) => {
+    layersData.categories.forEach(category => {
+      category.items.forEach(item => {
         if (item.active === undefined) {
           item.active = false; // Set default value if not present
         }
@@ -61,7 +55,7 @@ export class DataProviderUtils {
    * @returns Array of Asset objects containing the assets data
    */
   public readAssetsData(): AssetsDTO {
-    const fileContent = fs.readFileSync(this.assetsDataFilePath, "utf8");
+    const fileContent = fs.readFileSync(this.assetsDataFilePath, 'utf8');
     return JSON.parse(fileContent) as AssetsDTO;
   }
 
@@ -70,7 +64,7 @@ export class DataProviderUtils {
    * @returns GeoJSONDTO object containing the sample GeoJSON data
    */
   public readSampleGeoJsonData(): GeoJSONDTO {
-    const fileContent = fs.readFileSync(this.sampleGeoJsonFilePath, "utf8");
+    const fileContent = fs.readFileSync(this.sampleGeoJsonFilePath, 'utf8');
     return JSON.parse(fileContent) as GeoJSONDTO;
   }
 
@@ -79,7 +73,7 @@ export class DataProviderUtils {
    * @returns LocationsDTO array containing the substations data
    */
   public readSubstationsData(): LocationsDTO {
-    const fileContent = fs.readFileSync(this.substationsDataFilePath, "utf8");
+    const fileContent = fs.readFileSync(this.substationsDataFilePath, 'utf8');
     return JSON.parse(fileContent) as LocationsDTO;
   }
 
@@ -87,11 +81,11 @@ export class DataProviderUtils {
    * Read regions data from the JSON file and stores into local fuse instance
    */
   private readRegionsData() {
-    const fileContent = fs.readFileSync(this.regionsDataFilePath, "utf8");
+    const fileContent = fs.readFileSync(this.regionsDataFilePath, 'utf8');
     const regions = JSON.parse(fileContent) as SearchOptionDTO[];
 
     this.fuse = new Fuse(regions, {
-      keys: ["name"],
+      keys: ['name'],
       threshold: 0.3,
       distance: 100,
     });
@@ -101,14 +95,11 @@ export class DataProviderUtils {
    * @returns SearchOptionDTO array containing the matches relevant to the input string
    */
   public getSearchOptions(query: string): SearchOptionDTO[] {
-    if (!this.fuse) {
-      this.readRegionsData();
+    if(!this.fuse) {
+        this.readRegionsData();
     }
 
-    return this.fuse
-      .search(query)
-      .slice(0, 10)
-      .map((r) => r.item);
+    return this.fuse.search(query).slice(0, 10).map(r => r.item);
   }
 }
 
