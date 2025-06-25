22b602992812cb8440f33588d83716a9ba1f1407
kingarj <alice.king@informed.com>
2025-06-25 10:50:25 +0100

    feat: [DPAV-1002] address compilation issues

--- frontend/src/components/asset-marker/index.ts
+++ frontend/src/components/asset-marker/index.ts
@@ -1,2 +1,1 @@
 export { default } from './AssetMarker';
-export { default as AssetMarkerLayer } from './AssetMarkerLayer';
--- frontend/src/components/search/SearchPanel.spec.tsx
+++ frontend/src/components/search/SearchPanel.spec.tsx
@@ -92,7 +92,7 @@ describe('SearchPanel', () => {
     });
 
     it('renders search input and draw button by default', () => {
-        render(<SearchPanel mapRef={mockMapRef} showLayerControl={() => {}} hideLayerControl={() => {}} drawRef={mockDrawRef} />);
+        render(<SearchPanel mapRef={mockMapRef} showLayerControl={() => { } } hideLayerControl={() => { } } drawRef={mockDrawRef} isPanelOpen={false} setIsPanelOpen={() => { } } setPlacing={() => { } } />);
         expect(screen.getByTestId('search-input')).toBeInTheDocument();
         expect(screen.getByTestId('draw-polygon-button')).toBeInTheDocument();
     });
