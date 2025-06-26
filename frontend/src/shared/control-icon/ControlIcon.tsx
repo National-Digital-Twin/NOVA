424f0028a05c222bc5c50addc9b42236f5e01710
Sam Hill <sam.hill@informed.com>
2025-06-26 17:30:55 +0100

    1st batch of UI cleanup

--- frontend/src/shared/control-icon/ControlIcon.tsx
+++ frontend/src/shared/control-icon/ControlIcon.tsx
@@ -63,17 +63,9 @@ const ControlIcon = ({ onClick, children, 'aria-label': ariaLabel, isActive, dis
         <Box>
             {showTooltip ? (
                 <StyledTooltip title={ariaLabel}>
-                    <span>
-                        <StyledIconButton
-                            onClick={onClick}
-                            isActive={isActive}
-                            disabled={disabled}
-                            aria-pressed={ariaPressed}
-                            aria-label={ariaLabel + ' button'}
-                        >
-                            {children}
-                        </StyledIconButton>
-                    </span>
+                    <StyledIconButton onClick={onClick} isActive={isActive} disabled={disabled} aria-pressed={ariaPressed} aria-label={ariaLabel + ' button'}>
+                        {children}
+                    </StyledIconButton>
                 </StyledTooltip>
             ) : (
                 <StyledIconButton onClick={onClick} aria-label={ariaLabel} isActive={isActive} disabled={disabled} aria-pressed={ariaPressed}>
