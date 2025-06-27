507373a61a022d325be409b85d6a382a714f2d72
Sam Hill <sam.hill@informed.com>
2025-06-27 15:08:41 +0100

    theme update

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,12 +3,29 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#3670b3',
+            main: '#001f3f',
         },
         secondary: {
             main: '#3670b3',
         },
         divider: '#1c1c1c',
+    },
+    components: {
+        MuiButton: {
+            defaultProps: {
+                color: 'secondary',
+            },
+        },
+        MuiCheckbox: {
+            defaultProps: {
+                color: 'secondary',
+            },
+        },
+        MuiIconButton: {
+            defaultProps: {
+                color: 'secondary',
+            },
+        },
     },
 });
 
